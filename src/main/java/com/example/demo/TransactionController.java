73d474a238cbfe162067e0b559658a6a606388a8
YassineOujaa77 <yassineoujaa42@gmail.com>
2022-11-06 03:01:29 +0100

    edit url

--- src/main/java/com/example/demo/TransactionController.java
+++ src/main/java/com/example/demo/TransactionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v0/transaction")
+@RequestMapping(path = "api/v0/transaction")
 public class TransactionController {
 
     @Autowired
