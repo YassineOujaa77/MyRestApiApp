048cc3ac5683f1fdb07188c9fd6ec244a6f2db69
YassineOujaa77 <yassineoujaa42@gmail.com>
2022-11-06 03:19:06 +0100

    add getTransaction

--- src/main/java/com/example/demo/TransactionController.java
+++ src/main/java/com/example/demo/TransactionController.java
@@ -1,28 +1,24 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v0/transaction")
 public class TransactionController {
 
     @Autowired
     TransactionService transactionService;
 
-    @GetMapping
-    public Transaction getTransaction(@RequestParam int id  ){
+    @GetMapping("api/v0/transactions/{id}")
+    public Transaction getTransaction(@PathVariable int id  ){
 
         return transactionService.getTransactions().get(id);
 
     }
 
-    @GetMapping
+    @GetMapping("api/v0/transactions")
     public List<Transaction> getAllTransactions(){
         return transactionService.getTransactions();
     }
--- src/main/java/com/example/demo/TransactionService.java
+++ src/main/java/com/example/demo/TransactionService.java
@@ -11,11 +11,11 @@ public class TransactionService {
     public List<Transaction> getTransactions(){
         List<Transaction> transactions = new ArrayList<>();
 
-        transactions.add(new Transaction(1,"name1","150","15/12/22"));
-        transactions.add(new Transaction(2,"name2","2000","15/03/22"));
-        transactions.add(new Transaction(3,"name3","1771","10/07/22"));
-        transactions.add(new Transaction(4,"name4","1400","11/03/22"));
-        transactions.add(new Transaction(5,"name5","12","17/02/22"));
+        transactions.add(new Transaction(0,"name0","150","15/12/22"));
+        transactions.add(new Transaction(1,"name1","2000","15/03/22"));
+        transactions.add(new Transaction(2,"name2","1771","10/07/22"));
+        transactions.add(new Transaction(3,"name3","1400","11/03/22"));
+        transactions.add(new Transaction(4,"name4","12","17/02/22"));
 
         return transactions;
 
